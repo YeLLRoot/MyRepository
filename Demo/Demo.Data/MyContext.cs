ad1bf1b2885f6f35d16092e5923c3a220325ddcc
YeLLRoot <18437927328@163.com>
2019-05-28 15:48:39 +0800

    First commit

---
+++ Demo/Demo.Data/MyContext.cs
@@ -0,0 +1,17 @@
+﻿using Demo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Data
+{
+    public class MyContext:DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Movie> Movies { set; get; }
+    }
+}
---
+++ Demo/Demo.Web/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Demo.Web.Models;
+using Demo.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public MyContext dbcontext { get; }
+
+        public HomeController(MyContext context)
+        {
+            dbcontext = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await dbcontext.Movies.ToListAsync());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
